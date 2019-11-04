f60628a97061a79298242d791c58960d3e442870
sonna0909 <57337431+sonna0909@users.noreply.github.com>
2019-11-04 14:33:32 +0700

    Update topic.lean

--- topic.lean
+++ topic.lean
@@ -1,3 +1,3 @@
-[{"key":"alphabet","title":"Alphabet"},
-{"key":"number","title":"Number"},
-{"key":"color","title":"Color"}]
+[{"key":"alphabet","title":"Alphabet","spelling":"/ˈalfəˌbet/"},
+{"key":"number","title":"Number","spelling":"/ˈnəmbər/"},
+{"key":"color","title":"Color","spelling":"/ˈkələr/"}]
