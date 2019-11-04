ac2777c07e6cc2ecc7adc3e4a640c41dd85be387
sonna0909 <57337431+sonna0909@users.noreply.github.com>
2019-11-04 11:52:08 +0700

    Update alphabet.lean

--- topic/alphabet.lean
+++ topic/alphabet.lean
@@ -1,8 +1,8 @@
 [
     {"key":"a","title":"A","subs":[
-        {"key":"a1", "title":"a"},
+        {"key":"a1", "title":"a"}
         ]},
     {"key":"b","title":"B","subs":[
-            {"key":"b1", "title":"b"},
-        ]},
+            {"key":"b1", "title":"b"}
+        ]}
 ]
