e8883c7bfbe920ee54ecf04447508b540e25ff8f
sonna0909 <57337431+sonna0909@users.noreply.github.com>
2019-11-07 14:39:11 +0700

    Update alphabet.lean

--- topic/alphabet.lean
+++ topic/alphabet.lean
@@ -1,8 +1,54 @@
 [
-    {"key":"a","title":"A","subs":[
-        {"key":"a1", "title":"a"}
-        ]},
-    {"key":"b","title":"B","subs":[
-            {"key":"b1", "title":"b"}
-        ]}
+{"key":"a","title":"A","spelling":"/eɪ/","subs":[
+	{"key":"a", "title":"Apple","spelling":"/.../"}]},
+{"key":"b","title":"B","spelling":"/biː/","subs":[
+	{"key":"b", "title":"Ball","spelling":"/.../"}]},
+{"key":"c","title":"C","spelling":"/siː/","subs":[
+	{"key":"c", "title":"Cat","spelling":"/kæt/"}]},
+{"key":"d","title":"D","spelling":"/diː/","subs":[
+	{"key":"d", "title":"Dog","spelling":"/dɑːɡ;dɒɡ/"}]},
+{"key":"e","title":"E","spelling":"/iː/","subs":[
+	{"key":"e", "title":"Elephant","spelling":"/ˈel.ɪ.fənt/"}]},
+{"key":"f","title":"F","spelling":"/ɛf/","subs":[
+	{"key":"f", "title":"First","spelling":"/fə:st/"}]},
+{"key":"g","title":"G","spelling":"/dʒiː/","subs":[
+	{"key":"g", "title":"Giraffe","spelling":"/dʒɪˈrɑːf/"}]},
+{"key":"h","title":"H","spelling":"/eɪtʃ/","subs":[
+	{"key":"h", "title":"Horse","spelling":"/hoːs/"}]},
+{"key":"i","title":"I","spelling":"/aɪ/","subs":[
+	{"key":"i", "title":"In","spelling":"/in/"}]},
+{"key":"j","title":"J","spelling":"/dʒeɪ/","subs":[
+	{"key":"j", "title":"June","spelling":"/dʤu:n/"}]},
+{"key":"k","title":"K","spelling":"/keɪ/","subs":[
+	{"key":"k", "title":"Key","spelling":"/ki:/"}]},
+{"key":"l","title":"L","spelling":"/ɛl/","subs":[
+	{"key":"l", "title":"Learn","spelling":"/lə:n/"}]},
+{"key":"m","title":"M","spelling":"/ɛm/","subs":[
+	{"key":"m", "title":"Member","spelling":"/'membə/"}]},
+{"key":"n","title":"N","spelling":"/ɛn/","subs":[
+	{"key":"n", "title":"number","spelling":"/'nʌmbə/"}]},
+{"key":"o","title":"O","spelling":"/oʊ/","subs":[
+	{"key":"o", "title":"On","spelling":"/ɔn/"}]},
+{"key":"p","title":"P","spelling":"/piː/","subs":[
+	{"key":"p", "title":"Pay","spelling":"/pei/"}]},
+{"key":"q","title":"Q","spelling":"/kjuː/","subs":[
+	{"key":"q", "title":"Question","spelling":"/'kwest∫ən/"}]},
+{"key":"r","title":"R","spelling":"/ɑr/","subs":[
+	{"key":"r", "title":"Read","spelling":"/riːd/"}]},
+{"key":"s","title":"S","spelling":"/ɛs/","subs":[
+	{"key":"s", "title":"Sofa","spelling":"/’soufə/"}]},
+{"key":"t","title":"T","spelling":"/tiː/","subs":[
+	{"key":"t", "title":"Ten","spelling":"/ten/"}]},
+{"key":"u","title":"U","spelling":"/juː/","subs":[
+	{"key":"u", "title":"University","spelling":"/,ju:ni'və:siti/"}]},
+{"key":"v","title":"V","spelling":"/viː/","subs":[
+	{"key":"v", "title":"view","spelling":"/ˈvjuː/"}]},
+{"key":"w","title":"W","spelling":"/ˈdʌbəl.juː/","subs":[
+	{"key":"w", "title":"Write","spelling":"/rait/"}]},
+{"key":"x","title":"X","spelling":"/ɛks/","subs":[
+	{"key":"x", "title":"Xylophone","spelling":"/ksi.lɔ.fɔn/"}]},
+{"key":"y","title":"Y","spelling":"/waɪ/","subs":[
+	{"key":"y", "title":"Yes","spelling":"/jes/"}]},
+{"key":"z","title":"Z","spelling":"/zɛd/","subs":[
+	{"key":"z", "title":"Zoo","spelling":"/zu:/"}]}
 ]
