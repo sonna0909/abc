4483d247e3e4152eb9e60a3668af5c0d911d2849
sonna0909 <57337431+sonna0909@users.noreply.github.com>
2019-11-11 12:21:59 +0700

    Update alphabet.lean

--- topic/alphabet.lean
+++ topic/alphabet.lean
@@ -10,45 +10,45 @@
 {"key":"e","title":"E","spelling":"/iː/","subs":[
 	{"key":"elephant", "title":"Elephant","spelling":"/ˈel.ɪ.fənt/"}]},
 {"key":"f","title":"F","spelling":"/ɛf/","subs":[
-	{"key":"fish", "title":"Fish","spelling":"/fə:st/"}]},
+	{"key":"fish", "title":"Fish","spelling":"/fish/"}]},
 {"key":"g","title":"G","spelling":"/dʒiː/","subs":[
 	{"key":"giraffe", "title":"Giraffe","spelling":"/dʒɪˈrɑːf/"}]},
 {"key":"h","title":"H","spelling":"/eɪtʃ/","subs":[
 	{"key":"horse", "title":"Horse","spelling":"/hoːs/"}]},
 {"key":"i","title":"I","spelling":"/aɪ/","subs":[
-	{"key":"ice-cream", "title":"Ice cream","spelling":"/in/"}]},
+	{"key":"ice-cream", "title":"Ice cream","spelling":""}]},
 {"key":"j","title":"J","spelling":"/dʒeɪ/","subs":[
-	{"key":"jeep", "title":"Jeep","spelling":"/dʤu:n/"}]},
+	{"key":"jeep", "title":"Jeep","spelling":"/jēp/"}]},
 {"key":"k","title":"K","spelling":"/keɪ/","subs":[
 	{"key":"key", "title":"Key","spelling":"/ki:/"}]},
 {"key":"l","title":"L","spelling":"/ɛl/","subs":[
-	{"key":"lion", "title":"Lion","spelling":"/lə:n/"}]},
+	{"key":"lion", "title":"Lion","spelling":"/ˈlīən/"}]},
 {"key":"m","title":"M","spelling":"/ɛm/","subs":[
-	{"key":"monkey", "title":"Monkey","spelling":"/'membə/"}]},
+	{"key":"monkey", "title":"Monkey","spelling":"/ˈməngkē/"}]},
 {"key":"n","title":"N","spelling":"/ɛn/","subs":[
-	{"key":"nose", "title":"Nose","spelling":"/'nʌmbə/"}]},
+	{"key":"nose", "title":"Nose","spelling":"/nōz/"}]},
 {"key":"o","title":"O","spelling":"/oʊ/","subs":[
-	{"key":"orange", "title":"Orange","spelling":"/ɔn/"}]},
+	{"key":"orange", "title":"Orange","spelling":"/ˈôrənj,ˈärənj/"}]},
 {"key":"p","title":"P","spelling":"/piː/","subs":[
-	{"key":"pig", "title":"Pig","spelling":"/pei/"}]},
+	{"key":"pig", "title":"Pig","spelling":"/piɡ/"}]},
 {"key":"q","title":"Q","spelling":"/kjuː/","subs":[
-	{"key":"queen", "title":"Queen","spelling":"/'kwest∫ən/"}]},
+	{"key":"queen", "title":"Queen","spelling":"/kwēn/"}]},
 {"key":"r","title":"R","spelling":"/ɑr/","subs":[
-	{"key":"rabbit", "title":"Rabbit","spelling":"/riːd/"}]},
+	{"key":"rabbit", "title":"Rabbit","spelling":"/ˈrabət/"}]},
 {"key":"s","title":"S","spelling":"/ɛs/","subs":[
-	{"key":"snake", "title":"Snake","spelling":"/’soufə/"}]},
+	{"key":"snake", "title":"Snake","spelling":"/snāk/"}]},
 {"key":"t","title":"T","spelling":"/tiː/","subs":[
-	{"key":"table", "title":"Table","spelling":"/ten/"}]},
+	{"key":"table", "title":"Table","spelling":"/ˈtābəl/"}]},
 {"key":"u","title":"U","spelling":"/juː/","subs":[
-	{"key":"umbrella", "title":"Umbrella","spelling":"/,ju:ni'və:siti/"}]},
+	{"key":"umbrella", "title":"Umbrella","spelling":"/ˌəmˈbrelə/"}]},
 {"key":"v","title":"V","spelling":"/viː/","subs":[
-	{"key":"violin", "title":"Violin","spelling":"/ˈvjuː/"}]},
+	{"key":"violin", "title":"Violin","spelling":"/ˌvīəˈlin/"}]},
 {"key":"w","title":"W","spelling":"/ˈdʌbəl.juː/","subs":[
-	{"key":"window", "title":"Window","spelling":"/rait/"}]},
+	{"key":"window", "title":"Window","spelling":"/ˈwindō/"}]},
 {"key":"x","title":"X","spelling":"/ɛks/","subs":[
 	{"key":"xylophone", "title":"Xylophone","spelling":"/ksi.lɔ.fɔn/"}]},
 {"key":"y","title":"Y","spelling":"/waɪ/","subs":[
-	{"key":"yacht", "title":"Yacht","spelling":"/jes/"}]},
+	{"key":"yacht", "title":"Yacht","spelling":"/yät/"}]},
 {"key":"z","title":"Z","spelling":"/zɛd/","subs":[
-	{"key":"zebra", "title":"Zebra","spelling":"/zu:/"}]}
+	{"key":"zebra", "title":"Zebra","spelling":"/ˈzēbrə/"}]}
 ]
